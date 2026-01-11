7bb13724081d61960e7972de039590a728c61c15
leemonsharc <146793021+leemonsharc@users.noreply.github.com>
2026-01-11 15:50:33 -0800

    Fix LCDC flag for background display

--- src/assets/hello-world.asm
+++ src/assets/hello-world.asm
@@ -56,7 +56,7 @@ CopyTilemap:
 	jp nz, CopyTilemap
 
 	; Turn the LCD on
-	ld a, LCDC_ON | LCDCF_BG_ON
+	ld a, LCDC_ON | LCDC_BG_ON
 	ld [rLCDC], a
 
 	; During the first (blank) frame, initialize display registers
