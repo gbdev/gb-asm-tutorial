876e8c7be0279e90df83a20e9cb43ea28276f763
Les-Wet <iwetenkamp@icloud.com>
2026-03-19 14:45:36 +0100

    Add Project Organization Text to Title Screen Section (#168)
    * Remove unnecessary input.asm, fix style to match other asm files
    
    * Add code organization text to Title-Screen section
    
    * Change serial link wording for multi-object linking
    
    * Add justification for favoring smaller files

--- unbricked/title-screen/build.sh
+++ unbricked/title-screen/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+# ANCHOR: multibuild
 rgbasm -o main.o main.asm
 rgbasm -o input.o input.asm
 rgblink -o unbricked.gb main.o input.o
 rgbfix -v -p 0xFF unbricked.gb
+# ANCHOR_END: multibuild
